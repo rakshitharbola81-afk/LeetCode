bd98bfe6ee34f4b4e6b35181c06771cd400b8abc
RakshitHarbola <rakshitharbola81@gmail.com>
2026-09-12 11:14:21 +0530

    LC-number-of-enclaves [Runtime: N/A; Memory: N/A]

--- depth-first-search/number-of-enclaves.cpp
+++ depth-first-search/number-of-enclaves.cpp
@@ -32,7 +32,7 @@ public:
         }
         int cnt=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1 & vis[i][j]==0){
                     cnt++;
                 }
