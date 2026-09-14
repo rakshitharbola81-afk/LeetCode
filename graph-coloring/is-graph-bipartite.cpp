b5a70aaa710d858bf51f7ef7524a5c73ad7f23b2
RakshitHarbola <rakshitharbola81@gmail.com>
2026-09-14 11:43:30 +0530

    LC-is-graph-bipartite [Runtime: N/A; Memory: N/A]

---
+++ graph-coloring/is-graph-bipartite.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        queue<int>q;
+        q.push(0);
+        int n = graph.size();
+        vector<int> color(n, -1);
+        color[0]=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:graph[node]){
+                if(color[it]==-1){
+                    color[it]=!color[node];
+                    q.push(it);
+                }
+                else if(color[it]==color[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
