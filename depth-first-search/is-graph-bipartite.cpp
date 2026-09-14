4a740e398d4a7e56b08e342b06c98a11015c0de1
RakshitHarbola <rakshitharbola81@gmail.com>
2026-09-14 11:46:09 +0530

    LC-is-graph-bipartite [Runtime: N/A; Memory: N/A]

--- depth-first-search/is-graph-bipartite.cpp
+++ depth-first-search/is-graph-bipartite.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        queue<int>q;
-        q.push(0);
         int n = graph.size();
         int color[n];
         for(int i=0;i<n;i++)color[i]=-1;
         color[0]=0;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto it:graph[node]){
-                if(color[it]==-1){
-                    color[it]=!color[node];
-                    q.push(it);
-                }
-                else if(color[it]==color[node]){
-                    return false;
+        for (int i = 0; i < n; i++) {
+            if (color[i] != -1) continue;
+            queue<int> q;
+            q.push(i);
+            color[i] = 0;
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+                for (auto it : graph[node]) {
+                    if (color[it] == -1) {
+                        color[it] = !color[node];
+                        q.push(it);
+                    } else if (color[it] == color[node]) {
+                        return false;
+                    }
                 }
             }
         }
